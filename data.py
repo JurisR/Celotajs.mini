b6accbd73bf9cb6b4d9a62660b2ce74a1d36dab8
JurisR <49312083+JurisR@users.noreply.github.com>
2022-12-20 20:08:21 +0200

    more updates

--- data.py
+++ data.py
@@ -1,2 +1,93 @@
 import psycopg2
+import os
 
+DB_NAME = os.getenv("DB_NAME")
+DB_HOST = os.getenv("DB_HOST")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+
+dsn = "host={} dbname={} user={} password={}".format(DB_HOST, DB_NAME, DB_NAME,
+                                                     DB_PASSWORD)
+
+
+#table setup
+def create_table1():
+  conn = psycopg2.connect(dsn)
+  c = conn.cursor()
+  c.execute("""CREATE TABLE lietotaji
+                (user_id serial PRIMARY KEY, username varchar(10) UNIQUE NOT NULL, vards varchar(8) NOT NULL, uzvards varchar(8) NOT NULL);"""
+            )
+  conn.commit()
+  conn.close()
+  return "Table 1 ok"
+
+
+def create_table2():
+  conn = psycopg2.connect(dsn)
+  c = conn.cursor()
+  c.execute("""CREATE TABLE zinojumi
+                (user_id serial PRIMARY KEY,username VARCHAR(10), zinojums TEXT);"""
+            )
+  conn.commit()
+  conn.close()
+  return "Table 2 ok"
+
+
+def del_table(zinojumi):
+  conn = psycopg2.connect(dsn)
+  c = conn.cursor()
+  c.execute("""DROP TABLE zinojumi""")
+  conn.commit()
+  conn.close()
+  return "Table gone"
+
+##Adds user profile to database
+def add_user(username, vards, uzvards):
+  conn = psycopg2.connect(dsn)
+  c = conn.cursor()
+  c.execute(
+    """INSERT INTO lietotaji(username ,vards ,uzvards)   	                        
+    VALUES('{}','{}','{}');""".format(username, vards, uzvards))
+  c.close()
+  conn.commit()
+  conn.close()
+  return "User added"
+
+#Adds message entry to database
+def add_entry(user_id, zinojums):
+  conn = psycopg2.connect(dsn)
+  c = conn.cursor()
+  c.execute(
+    """INSERT INTO zinojumi(user_id,zinojums)VALUES('{}','{}');""".format(user_id, zinojums))
+  c.close()
+  conn.commit()
+  conn.close()
+  return "Entry ok"
+
+#Calls Database to recieve user_id and username
+def get_user_and_id():
+  conn = psycopg2.connect(dsn)
+  c = conn.cursor()
+  c.execute(""" SELECT user_id, username FROM lietotaji  """)
+  atbilde = c.fetchall()
+  c.close()
+  conn.commit()
+  conn.close()
+  return atbilde
+
+def getName_and_id():
+  conn = psycopg2.connect(dsn)
+  c = conn.cursor()
+  c.execute("""
+SELECT
+    vards,
+    uzvards,
+    zinojums
+FROM
+    lietotaji
+JOIN zinojumi
+    ON lietotaji.user_id = zinojumi.user_id;""")
+  saraksts = c.fetchall()
+  c.close()
+  conn.commit()
+  conn.close()
+  return saraksts
--- main.py
+++ main.py
@@ -1,15 +1,75 @@
 from flask import Flask, render_template, json, request
 
 app = Flask(__name__)
+from flask import Flask, render_template, json, request, jsonify
+from data import *
+app = Flask(__name__)
+
+# print(create_table2())
+
+# print(del_table())
+
+# print(add_entry("BeanMann","Juris","Redmanis","Labdien!"))
+
+# print(add_user("JOJO","Jonathan","Jostar"))
+
+@app.route('/get_user')
+def passON():
+  data = get_user_and_id()
+  answer = []
+  for element in data:
+    answer.append({
+    "id":element[0],
+    "username":element[1]})
+  return jsonify({"all":answer})
 
 
-@app.route('/post')
-def CC_page():
-    return render_template("createpost.html")
-  
+@app.route('/register')
+def create_page1():
+    return render_template("zin.register.html")
+
+@app.route('/pievienot')
+def create_page2():
+  return render_template("zin.piev.html")
+
 @app.route('/')
-def index_page():
-  return render_template("celotajs.html")
+def create_page3():
+  fullList=[]
+  data = getName_and_id() # Dabūt visus vārdus, uzvārdus un ziņas
+  for element in data:
+    fullList.append({
+    "vards":element[0],
+    "uzvards":element[1],
+    "MSG" : element[2]})# Salikt sarakstā
+  return render_template("index.html", visasZinas=fullList)
+
+#Nolasa html->.JS ierakstīto lietotajvardu, vardu un uzvardu un pievieno datubāzei.
+@app.route('/enterUSR',methods=["POST"])
+def lasit():
+  dati=request.json
+  print(dati)
+  return add_user(dati["username"], dati["vards"], dati["uzvards"])
+
+#Nolasa html->.JS ierakstīto Ziņojumu un Kopā ar izvēlēto lietotāju to pievieno datubāzei.
+
+@app.route('/enterMSG',methods=["POST"])
+def lasit2():
+  dati=request.json
+  print(dati)
+  return add_entry(dati["user_id"], dati["zinojums"])
+
+@app.route('/show_msg')
+def passMSG():
+  data = getName_and_id() 
+  Listfull = []
+  for element in data:
+    Listfull.append({
+    "vards":element[0],
+    "uzvards":element[1],
+    "MSG" : element[2]})
+  return ({"all":Listfull})
+
+
 
 
 app.run(host='0.0.0.0', port=81)
