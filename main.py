0106d7e1141b65d43b5d95c5c50b9b1fbcc8f3f9
JurisR1 <49312083+JurisR@users.noreply.github.com>
2022-12-20 10:06:49 +0000

    Added templates

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template, json, request
 
 app = Flask(__name__)
 
 
+@app.route('/post')
+def CC_page():
+    return render_template("createpost.html")
+  
 @app.route('/')
-def index():
-    return 'Hello from Flask!'
+def index_page():
+  return render_template("celotajs.html")
 
 
 app.run(host='0.0.0.0', port=81)
